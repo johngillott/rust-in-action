2d905351142a380adbedfa4c97ab0375e88694e9
John Gillott <jsgtwenty@gmail.com>
2020-07-08 19:00:31 +0100

    grep-lite using regex pkg to search strings

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,12 +1,18 @@
+extern crate regex;
+
+use regex::Regex;
+
 fn main() {
-    let search_term = "picture";
+    let re = Regex::new("pictures").unwrap();
+
     let quote = "Every face, every shop, bedroom window, public-house, and
     dark square is a picture feverishly turned--in search of what?
     It is the same with books. What do we seek through millions of pages?";
 
     for line in quote.lines() {
-        if line.contains(search_term) {
-            println!("{}", line);
+        match re.find(line) {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
